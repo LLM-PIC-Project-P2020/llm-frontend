8632f81f8fd48b6e42f1d1bb7a2a9c59faddb14f
Lee <vincent_li_2024@126.com>
2025-01-13 21:52:53 +0800

    Fix wrong building instruction

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
 
-import {route, type RouteConfig} from "@react-router/dev/routes"
+import {route, index, type RouteConfig} from "@react-router/dev/routes"
 
 export default [
-    route("/", "./Mainpage.tsx"),
+    index("./Mainpage.tsx"),
     route("/playground", "./Playground.tsx"),
     route("*?", "catchall.tsx"),
 ] satisfies RouteConfig

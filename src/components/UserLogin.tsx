386ed7715c192a49836da2cf316312dbb3d1efbe
Lee <vincent_li_2024@126.com>
2025-05-19 13:08:46 +0800

    Add UserLogin submit action.

--- src/components/UserLogin.tsx
+++ src/components/UserLogin.tsx
@@ -1,6 +1,7 @@
-import { Button, ButtonGroup, Classes, Dialog, DialogBody, DialogFooter, FormGroup, InputGroup, Tooltip } from "@blueprintjs/core";
-import { useState } from "react";
+import { Button, Classes, Dialog, DialogBody, DialogFooter, FormGroup, InputGroup, Tooltip } from "@blueprintjs/core";
+import { FormEvent, useRef, useState } from "react";
 import UserRegistration from "./UserRegistration";
+import { components } from "../../api/openapi";
 
 function RegisterButton(openRegistrationForm : () => void) {
     return (<Tooltip content={"注册"}>
@@ -19,10 +20,30 @@ function TogglePasswordDisplay({isClearText, setClearText} : {isClearText: boole
 function UserLogin({isOpen, setOpen} : { isOpen : boolean, setOpen : (arg0: boolean) => void})  {
     const [isClearText, setClearText] = useState(false);
     const [isRegistrationShown, setRegistrationShown] = useState(false);
+    const userDataRef = useRef<components['schemas']['LoginRequest']>({});
+
+    const handleSubmit = async (e : FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.assert(userDataRef.current != null);
+    
+        try {
+            console.dir(userDataRef.current);
+            const response = await fetch('/api/sessions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userDataRef.current)
+            });
+            console.log("Successful: " + response.ok);
+        } catch (e) {
+            console.log("Failed: " + e);
+        }
+    };
 
     const footerActions = <>
         <Button intent="warning" text="忘记密码" />
-        <Button intent="primary" text="登录" rightIcon="arrow-right"/>
+        <Button intent="primary" type="submit" text="登录" rightIcon="arrow-right"/>
     </>;
     
     return (<Dialog 
@@ -32,17 +53,27 @@ function UserLogin({isOpen, setOpen} : { isOpen : boolean, setOpen : (arg0: bool
         usePortal
         title="登录"
     >
-        <DialogBody>
-            <FormGroup label="账户" labelFor="text-input">
-                <InputGroup rightElement={RegisterButton(() => setRegistrationShown(true))}  style={{flexGrow: 1}}/>
-            </FormGroup>
+        <form onSubmit={handleSubmit}>
+            <DialogBody>
+                <FormGroup label="账户" labelFor="text-input">
+                    <InputGroup 
+                        rightElement={RegisterButton(() => setRegistrationShown(true))}  
+                        style={{flexGrow: 1}}
+                        onChange={(e) => userDataRef.current.id = e.target.value}
+                    />
+                </FormGroup>
 
-            <FormGroup label="密码" labelFor="text-input">
-                <InputGroup type={isClearText ? "text" : "password"} rightElement={TogglePasswordDisplay({isClearText, setClearText})}/>
-            </FormGroup>
-        </DialogBody>
+                <FormGroup label="密码" labelFor="text-input">
+                    <InputGroup 
+                        type={isClearText ? "text" : "password"} 
+                        rightElement={TogglePasswordDisplay({isClearText, setClearText})}
+                        onChange={(e) => userDataRef.current.password = e.target.value}
+                    />
+                </FormGroup>
+            </DialogBody>
 
-        <DialogFooter actions={footerActions} />
+            <DialogFooter actions={footerActions} />
+        </form>
 
         <UserRegistration isOpen={isRegistrationShown} setOpen={setRegistrationShown} />
     </Dialog>);

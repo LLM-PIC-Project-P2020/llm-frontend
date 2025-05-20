f55f40f0f5317d37f13d6a66834649c97dff2149
Lee <vincent_li_2024@126.com>
2025-05-20 14:08:04 +0800

    Add class page layout.

--- src/components/Courses/Class/index.tsx
+++ src/components/Courses/Class/index.tsx
@@ -1,8 +1,33 @@
 import { useParams } from "react-router";
+import './class.css';
+import NavigationBar from "../../Navbar";
 
 function Class() {
     const { courseId, classId } = useParams();
-    return <div>You are accessing {courseId} with {classId}.</div>;
+    return <>
+        <NavigationBar />
+        <div className="class-app">
+            <div className="class-nav-wrapper">
+                <div className="class-nav">
+                    <ul>
+                        <li>aaa</li>
+                        <li>aaa</li>
+                        <li>aaa</li>
+                        <li>aaa</li>
+                    </ul>
+                </div>
+            </div>
+            <main className="class-main-wrapper">
+                <div className="class-main">
+                    <h1>Title</h1>
+                    <p>paragraphs.</p>
+                    <h2>Subtitle</h2>
+                    <p>You are accessing course {courseId}.</p>
+                    {classId && <p>The class you are accessing is {classId}.</p>}
+                </div>
+            </main>
+        </div>
+    </>;
 }
 
 export default Class;
